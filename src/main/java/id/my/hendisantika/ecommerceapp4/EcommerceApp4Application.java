63846360b89292edd4d79547da28bfc0bae717a6
hendisantika <hendisantika@yahoo.co.id>
2025-09-16 21:01:56 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp4/EcommerceApp4Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.ecommerceapp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApp4Application.class, args);
+    }
+
+}
