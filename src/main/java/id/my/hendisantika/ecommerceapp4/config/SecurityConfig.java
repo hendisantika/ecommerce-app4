23dd025b199469800aa3b2e3e14575dbd38c497c
hendisantika <hendisantika@yahoo.co.id>
2025-09-21 06:27:10 +0700

    feat: Add configure in SecurityConfig🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/ecommerceapp4/config/SecurityConfig.java
+++ src/main/java/id/my/hendisantika/ecommerceapp4/config/SecurityConfig.java
@@ -1,13 +1,26 @@
 package id.my.hendisantika.ecommerceapp4.config;
 
+import id.my.hendisantika.ecommerceapp4.entity.User;
 import id.my.hendisantika.ecommerceapp4.repository.UserRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -48,5 +61,88 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**")
+                .hasRole("ADMIN")
+                .antMatchers("/customer/**")
+                .hasRole("CUSTOMER")
+                .antMatchers("/seller/**")
+                .hasRole("SELLER")
+                .antMatchers("/**")
+                .permitAll()
+                .and()
+                .formLogin()
+                .successHandler(new AuthenticationSuccessHandler() {
+
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication) throws IOException, ServletException {
+
+                        User user = userRepo.loadUserByUserName(authentication.getName());
+
+                        String redirectURL = request.getContextPath();
+
+                        if (user.getRole().equals("ROLE_CUSTOMER")) {
+                            redirectURL = "/customer/home";
+                        }
+                        if (user.getRole().equals("ROLE_SELLER")) {
+                            redirectURL = "/seller/home";
+                        }
+                        if (user.getRole().equals("ROLE_ADMIN")) {
+                            redirectURL = "/admin/home";
+                        }
+
+                        response.sendRedirect(redirectURL);
+
+                    }
+                })
+                .failureHandler(new AuthenticationFailureHandler() {
+
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                                        AuthenticationException exception) throws IOException, ServletException {
+
+                        HttpSession httpSession = request.getSession();
+
+                        if (exception.getMessage().equals("Bad credentials")) {
+                            httpSession.setAttribute("status", "bad-credentials");
+                            response.sendRedirect("/login?=BadCredentials");
+                            return;
+                        }
+
+                        if (exception.getMessage().equals("User is disabled")) {
+                            httpSession.setAttribute("status", "user-disabled");
+                            response.sendRedirect("/login?=AccountSuspended");
+                        }
+
+                    }
+                })
+                .loginPage("/login")
+                .and()
+                .logout()
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+                            throws IOException, ServletException {
+
+                        HttpSession httpSession = request.getSession();
+
+                        if (authentication != null) {
+                            httpSession.setAttribute("status", "logout-success");
+                            response.sendRedirect("/login?logoutSuccess");
+                        } else {
+                            response.sendRedirect("/login?doLogin");
+                        }
+
+                    }
+                })
+                .and()
+                .csrf()
+                .disable();
     }
 }
