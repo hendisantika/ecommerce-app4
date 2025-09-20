8379c3b72ed0051b8b52d570dce226c40f5025da
hendisantika <hendisantika@yahoo.co.id>
2025-09-21 06:21:59 +0700

    feat: Add OrderRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp4/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.ecommerceapp4.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/09/25
+ * Time: 06.20
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    @Query(value = "SELECT * FROM orders WHERE product_id = ?", nativeQuery = true)
+    Order findByProductId(@Param("id") Integer id);
+}
