a7eaf923601d2370325e8c2e0938766ae28a2562
hendisantika <hendisantika@yahoo.co.id>
2025-09-21 06:15:45 +0700

    feat: Add Category Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp4/entity/Category.java
@@ -0,0 +1,47 @@
+package id.my.hendisantika.ecommerceapp4.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/09/25
+ * Time: 06.14
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "category_title", unique = true)
+    @NotBlank(message = "Category title is required.")
+    private String title;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products = new ArrayList<>();
+}
