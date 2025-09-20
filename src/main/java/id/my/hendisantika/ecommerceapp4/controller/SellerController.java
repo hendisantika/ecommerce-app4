96d48b136f96be7558f2a7c717e987b117c5ccf7
hendisantika <hendisantika@yahoo.co.id>
2025-09-21 06:42:41 +0700

    feat: Add SellerController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/ecommerceapp4/controller/SellerController.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.ecommerceapp4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ecommerce-app4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/09/25
+ * Time: 06.42
+ * To change this template use File | Settings | File Templates.
+ */
+@Controller
+@RequestMapping("/seller")
+public class SellerController {
+}
